e507a5d3851bec8687fd7b6ce0cc99ecd267b735
Mohammad <mo.litkouhi@gmail.com>
2022-08-22 12:09:38 -0700

    Added some lines of code.

--- main.js
+++ main.js
@@ -6,3 +6,19 @@ const navbar_scroll = () => {
 };
 
 window.addEventListener('scroll', navbar_scroll);
+
+// JS FUNCTION TO OPEN AND CLOSE THE ANSWER BOXES IN FAQs SECTION
+const getFaq = document.querySelectorAll('.faq');
+
+// ADD EVENTLISTENER FOR EACH ELEMENT
+getFaq.forEach((elem) => {
+  elem.addEventListener('click', () => {
+    elem.classList.toggle('open');
+
+    // CHANGE ICON
+    const icon = elem.querySelector('.faq__icon i');
+    icon.className === 'fa-solid fa-plus'
+      ? (icon.className = 'fa-solid fa-minus')
+      : (icon.className = 'fa-solid fa-plus');
+  });
+});
